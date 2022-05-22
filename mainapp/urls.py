40c66af12c39c347ff2acdafc1543f8657cea297
Vladislav.Semenenya <vladsmnnya@gmail.com>
2022-05-22 16:31:41 +0300

    lesson_2 changes

---
+++ mainapp/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from mainapp.apps import MainappConfig
+from mainapp import views
+
+
+app_name = MainappConfig.name
+
+urlpatterns = [
+    path('contacts/', views.ContactsView.as_view()),
+    path('courses/', views.CoursesListView.as_view()),
+    path('docsite/', views.DocSiteView.as_view()),
+    path('', views.IndexView.as_view()),
+    path('login/', views.LoginView.as_view()),
+    path('news/', views.NewsView.as_view()),
+]
+
+
+
