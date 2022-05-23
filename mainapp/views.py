9fe9eced9f66f3fd6c3c6ee969826300544616f5
Vladislav.Semenenya <vladsmnnya@gmail.com>
2022-05-23 21:09:04 +0300

    lesson 3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
+from datetime import datetime
 
 
 class ContactsView(TemplateView):
     template_name = 'mainapp/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['contacts'] = [
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHcrhA',
+                'city': 'Санкт‑Петербург',
+                'phone': '+7-999-11-11111',
+                'email': 'geeklab@spb.ru',
+                'address': 'территория Петропавловская крепость, 3Ж',
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHX3xB',
+                'city': 'Казань',
+                'phone': '+7-999-22-22222',
+                'email': 'geeklab@kz.ru',
+                'address': 'территория Кремль, 11, Казань, Республика Татарстан, Россия',
+            },
+            {
+                'map': 'https://yandex.ru/map-widget/v1/-/CCUAZHh9kD',
+                'city': 'Москва',
+                'phone': '+7-999-33-33333',
+                'email': 'geeklab@msk.ru',
+                'address': 'Красная площадь, 7, Москва, Россия',
+            },
+        ]
+
+        return context_data
 
 
 class CoursesListView(TemplateView):
@@ -25,3 +52,35 @@ class LoginView(TemplateView):
 
 class NewsView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = [
+            {
+                'title': 'Новость',
+                'preview': 'Предворительное описание новости',
+                'date': datetime.now()
+            },
+            {
+                'title': 'Новость',
+                'preview': 'Предворительное описание новости',
+                'date': datetime.now()
+            },
+            {
+                'title': 'Новость',
+                'preview': 'Предворительное описание новости',
+                'date': datetime.now()
+            },
+            {
+                'title': 'Новость',
+                'preview': 'Предворительное описание новости',
+                'date': datetime.now()
+            },
+            {
+                'title': 'Новость',
+                'preview': 'Предворительное описание новости',
+                'date': datetime.now()
+            },
+        ]
+
+        return context_data
